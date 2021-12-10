1fd4d5e2b233b66a6da15fdf0ff5951df6929734
lost731 <lost731@naver.com>
2021-12-10 23:32:25 +0900

    Add: Category-List

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,26 @@ import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobayStyle'
 import Introduction from 'components/Main/Introduction'
 import Footer from 'components/Common/Footer'
+import CategoryList from 'components/Main/CategoryList'
+
+const CATEGORY_LIST = {
+  All: 3,
+  React: 2,
+  PWA: 2,
+}
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
 `
+
 const IndexPage: FunctionComponent = function () {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="All" categoryList={CATEGORY_LIST} />
       <Footer />
     </Container>
   )

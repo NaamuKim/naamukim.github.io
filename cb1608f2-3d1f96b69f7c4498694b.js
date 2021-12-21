34c9971b4fcdf7ed761ced7d439a99db5eff460d
lost731 <lost731@naver.com>
2021-12-21 23:48:17 +0900

    Updates

---
+++ cb1608f2-3d1f96b69f7c4498694b.js
@@ -0,0 +1,3 @@
+/*! For license information please see cb1608f2-3d1f96b69f7c4498694b.js.LICENSE.txt */
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[523],{8014:function(c,t,a){a.d(t,{acZ:function(){return e}});var e={prefix:"fas",iconName:"arrow-left",icon:[448,512,[],"f060","M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"]}}}]);
+//# sourceMappingURL=cb1608f2-3d1f96b69f7c4498694b.js.map
